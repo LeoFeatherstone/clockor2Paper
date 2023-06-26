a302679bbcb553917f342a2f5f327e0b21ecb3b1
Leo <58927485+LeoFeatherstone@users.noreply.github.com>
2023-06-27 09:59:33 +1000

    update timing data

--- bfrTiming/testTrees.R
+++ bfrTiming/testTrees.R
@@ -26,7 +26,7 @@ for (i in seq_along(trees)) {
 # on Lenovo Thinkpad with 11th Gen intel i7 processor
 # TempEst v1.5.3
 # chromium v111.0.5563.64
-time <- data.frame(
+R2 <- data.frame(
     nTips =        c(100,      500,     1000,      5000,    10000),
     clockor2Time = c("0.47", "2.609", "7.819" , "235.979",   "1121.833"    ),
     tempestTime  = c("< 1",   "1.52",  "5.51",    "166.33", "820.0")
@@ -35,18 +35,32 @@ time <- data.frame(
 # on mac M1
 # chrome v 113.0.5627.126
 # tempest v1.5.3
-time <- data.frame(
+R2 <- data.frame(
     nTips =        c(100,      500,     1000,      5000,    10000),
     clockor2Time = c("0.26",  "1.53", "5.35",    "189.02",  "422.11"),
-    tempestTime  = c("< 1",    "2.40", "10.28",  "272.29", "1310.34")
+    tempestTime  = c("0.76",    "2.40", "10.28",  "272.29", "1310.34")
 )
 
-# try plot
-time <- data.frame(
+RMS <- data.frame(
     nTips =        c(100,      500,     1000,      5000,    10000),
-    clockor2Time = as.numeric(c("0.26",  "1.53", "5.35",    "189.02",  "1082.63")),
-    tempestTime  = as.numeric(c("0.26",    "2.40", "10.28",  "272.29", "1310.34"))
+    tempestTime = c("0.05",    "1.5",   "5.43",    "122",  "951.00"),
+    clockor2Time  = c("0.17",    "0.964", "2.782",  "113.581", "698.065")
 )
-# # if plot
-# time <- time %>% 
-#     pivot_longer(cols = ends_with("Time"), names_to = "Tool", values_to = "Time")
+
+tab <- bind_rows("R-Squared" = R2, "Residual Mean Squared" = RMS, .id = "Objective") %>% 
+    pivot_longer(cols = ends_with("Time"), names_to = "Tool", values_to = "Time") %>%
+    mutate(Time = as.numeric(Time)) %>%
+    pivot_wider()
+
+print(xtable::xtable(RMS, type = "latex"), file = "rmsTime.tex")
+print(xtable::xtable(R2, type = "latex"), file = "r2Time.tex")
+
+
+# try plot
+bind_rows("R-Squared" = R2, "Residual Mean Squared" = RMS, .id = "Objective") %>% 
+    pivot_longer(cols = ends_with("Time"), names_to = "Tool", values_to = "Time") %>%
+    mutate(Time = as.numeric(Time)) %>%
+    ggplot() +
+    geom_line(aes(x = nTips, y = Time, col = Tool, lty = Objective), size = 2) +
+    scale_y_continuous(trans = "log10") +
+    scale_color_manual(values = c("red", "dodgerblue"))
